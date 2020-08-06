2d7ac573c168d48a3014aad7d4bb36663fb162d1
Vaishnavi P <vaishnavip@prdxn.com>
2020-08-06 17:26:16 +0530

    single post page

---
+++ wp-content/themes/august/single-genres.php
@@ -0,0 +1,14 @@
+<?php 
+get_header(); 
+if(have_posts()) {
+    while(have_posts()) {
+      the_post(); ?>
+        <div class="wrapper">
+          <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+          
+        </div>
+        <?php
+    }  
+}
+get_footer();
+?>
